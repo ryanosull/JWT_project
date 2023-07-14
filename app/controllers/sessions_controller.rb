8a31903017d36aeafd0a123efaf0cf60499f1a2e
William Ryan OSullivan <wrosullivan@gmail.com>
2023-07-14 10:08:16 -0500

    hmm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,14 @@
 class SessionsController < ApplicationController
 
-
     def create
         @user = User.find_by(email: params[:email])
         if @user and @user.authenticate(params[:password])
             logged_user = JWT.encode({user: @user.id}, ENV['JWT_TOKEN'])
-            render json: logged_user, status: :ok #200
+            render json: {uid: logged_user}, status: :ok #200
         else 
             render json: {errors: ['Invalid email address and/or password!']}, status: :unauthorized #401
         end
     end
 
-
-
     #end
 end
-
-

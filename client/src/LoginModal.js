fce4a2a8071f16461c5a32db682350e8650183c7
William Ryan OSullivan <wrosullivan@gmail.com>
2023-07-15 04:37:55 -0500

    cleanup password info in modal

--- client/src/LoginModal.js
+++ client/src/LoginModal.js
@@ -4,13 +4,14 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
 function LoginModal() {
-    
+
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     return (
+
     <div> 
         <Button variant="primary" onClick={handleShow}> Click to Login</Button>
 
@@ -24,18 +25,19 @@ function LoginModal() {
 
             <Form>
 
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email"placeholder="name@example.com" autoFocus/>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email"placeholder="name@example.com" autoFocus/>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="password" />
-                <Form.Text className="text-muted">
-                    Password must be between 8 and 16 characters and contain at least one number, upper case letter, lower case letter, and symbol. Passwords are case sensitive!
-                </Form.Text>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="password" />
+                    <br/>
+                    <Form.Text className="text-muted">
+                        Password must be between 8 and 16 characters and contain at least one number (9), upper case letter (Z), lower case letter (a), and symbol (!). Passwords are case sensitive!
+                    </Form.Text>
+                </Form.Group>
 
             </Form>
 
@@ -48,6 +50,7 @@ function LoginModal() {
 
         </Modal>
     </div>
+
     );
 };
 
